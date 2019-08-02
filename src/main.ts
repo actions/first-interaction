75195489020d30dc12a18d79f6f03467630411c4
Danny McCormick <damccorm@microsoft.com>
2019-08-02 14:44:56 -0400

    Get it working

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,71 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput} from inside a container`);
-
-    // Get github context data
+    // Get client and context
+    const client = new github.GitHub(core.getInput('repoToken', { required: true }));
     const context = github.context;
-    console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
+
+    // Do nothing if its not a pr or issue
+    const isIssue = !!context.payload.issue;
+    if (!isIssue && !context.payload.pullRequest) {
+      core.setNeutral('Not a pull request or issue');
+      return;
+    }
+
+    // Do nothing if its not their first contribution
+    const sender = context.payload.sender.login;
+    const firstContribution = isIssue ? await isFirstIssue(client, context, isIssue, sender) : await isFirstPull(client, context, isIssue, sender);
+    if (!firstContribution) {
+      core.setNeutral('Not the users first contribution');
+      return;
+    }
+    
+    // Do nothing if no message set for this type of contribution
+    const message = isIssue ? core.getInput('issueMessage') : core.getInput('prMessage');
+    if (!message) {
+      core.setNeutral('No message provided for this type of contribution');
+    }
+
+    // Add a comment to the appropriate place
+    const issue = context.issue;
+    await client.issues.createComment(issue.owner, issue.repo, issue.number, message);
+
   } catch (error) {
     core.setFailed(error.message);
+    return;
   }
+}
+
+async function isFirstIssue(client, owner, repo, sender): Promise<boolean> {
+  const {status, data: issues} = await client.issues.listForRepo({owner: owner, repo: repo, creator: sender, state: 'all'});
+
+  if (status !== 200) {
+    throw new Error(`Received API status code ${status}`);
+  }
+
+  return issues.length === 0;
+}
+
+// No way to filter pulls by creator
+async function isFirstPull(client, owner, repo, sender, page = 1): Promise<boolean> {
+  const {status, data: pulls} = await client.pulls.list({owner: owner, repo: repo, per_page: 100, page: page, state: 'all'});
+
+  if (status !== 200) {
+    throw new Error(`Received API status code ${status}`);
+  }
+
+  if (pulls.length === 0) {
+    return true;
+  }
+
+  for (const pull of pulls) {
+    const login = pull.user.login;
+    if (login === sender) {
+      return false;
+    }
+  }
+
+  return await isFirstPull(client, owner, repo, sender, page+1);
 }
 
 run();
