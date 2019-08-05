6fdea0bd535d4a734871de53a43726c62305ce92
Danny McCormick <damccorm@microsoft.com>
2019-08-05 08:42:37 -0400

    Dont validate issues against prs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ const fs = require('fs');
 async function run() {
   try {
     // Get client and context
-    const client = new github.GitHub(core.getInput('repoToken', { required: true }));
+    const client = new github.GitHub(
+      core.getInput('repoToken', {required: true})
+    );
     const context = github.context;
 
     if (context.payload.action !== 'opened') {
@@ -24,14 +26,30 @@ async function run() {
     console.log('Checking if its the users first contribution');
     const sender = context.payload.sender.login;
     const issue = context.issue;
-    const firstContribution = isIssue ? await isFirstIssue(client, issue.owner, issue.repo, sender, issue.number) : await isFirstPull(client, issue.owner, issue.repo, sender, issue.number);
+    const firstContribution = isIssue
+      ? await isFirstIssue(
+          client,
+          issue.owner,
+          issue.repo,
+          sender,
+          issue.number
+        )
+      : await isFirstPull(
+          client,
+          issue.owner,
+          issue.repo,
+          sender,
+          issue.number
+        );
     if (!firstContribution) {
       console.log('Not the users first contribution');
       return;
     }
-    
+
     // Do nothing if no message set for this type of contribution
-    const message = isIssue ? core.getInput('issueMessage') : core.getInput('prMessage');
+    const message = isIssue
+      ? core.getInput('issueMessage')
+      : core.getInput('prMessage');
     if (!message) {
       console.log('No message provided for this type of contribution');
       return;
@@ -40,21 +58,40 @@ async function run() {
     // Add a comment to the appropriate place
     console.log(`Adding message: ${message}`);
     if (isIssue) {
-      await client.issues.createComment({ owner: issue.owner, repo: issue.repo, issue_number: issue.number, body: message });
+      await client.issues.createComment({
+        owner: issue.owner,
+        repo: issue.repo,
+        issue_number: issue.number,
+        body: message
+      });
+    } else {
+      await client.pulls.createReview({
+        owner: issue.owner,
+        repo: issue.repo,
+        pull_number: issue.number,
+        body: message,
+        event: 'COMMENT'
+      });
     }
-    else {
-      await client.pulls.createReview({ owner: issue.owner, repo: issue.repo, pull_number: issue.number, body: message, event: 'COMMENT' });
-    }
-    
-
   } catch (error) {
     core.setFailed(error.message);
     return;
   }
 }
 
-async function isFirstIssue(client, owner, repo, sender, number): Promise<boolean> {
-  const {status, data: issues} = await client.issues.listForRepo({owner: owner, repo: repo, creator: sender, state: 'all'});
+async function isFirstIssue(
+  client,
+  owner,
+  repo,
+  sender,
+  number
+): Promise<boolean> {
+  const {status, data: issues} = await client.issues.listForRepo({
+    owner: owner,
+    repo: repo,
+    creator: sender,
+    state: 'all'
+  });
 
   if (status !== 200) {
     throw new Error(`Received API status code ${status}`);
@@ -66,7 +103,7 @@ async function isFirstIssue(client, owner, repo, sender, number): Promise<boolea
 
   for (const issue of issues) {
     const issueNumber = issue.number;
-    if (issueNumber < number) {
+    if (issueNumber < number && !issue.pull_request) {
       return false;
     }
   }
@@ -75,9 +112,22 @@ async function isFirstIssue(client, owner, repo, sender, number): Promise<boolea
 }
 
 // No way to filter pulls by creator
-async function isFirstPull(client, owner, repo, sender, number, page = 1): Promise<boolean> {
+async function isFirstPull(
+  client,
+  owner,
+  repo,
+  sender,
+  number,
+  page = 1
+): Promise<boolean> {
   console.log('Checking...');
-  const {status, data: pulls} = await client.pulls.list({owner: owner, repo: repo, per_page: 100, page: page, state: 'all'});
+  const {status, data: pulls} = await client.pulls.list({
+    owner: owner,
+    repo: repo,
+    per_page: 100,
+    page: page,
+    state: 'all'
+  });
 
   if (status !== 200) {
     throw new Error(`Received API status code ${status}`);
@@ -95,7 +145,7 @@ async function isFirstPull(client, owner, repo, sender, number, page = 1): Promi
     }
   }
 
-  return await isFirstPull(client, owner, repo, sender, number, page+1);
+  return await isFirstPull(client, owner, repo, sender, number, page + 1);
 }
 
 run();
